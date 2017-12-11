626f95392572fc40374bf73ed7f30a005ca720dc
Manjula Jayawardana <manjula.jayawardana@seebo.com.au>
2017-12-11 13:32:09 +0530

    initial commit

---
+++ src/main/java/com/manjula/springangular/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.manjula.springangular;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringangularApplication.class);
+	}
+
+}
